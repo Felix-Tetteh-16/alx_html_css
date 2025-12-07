b28d72f59e8691c9d240e5a662d8d0aa24601357
Felix Tetteh <felixdev48@gmail.com>
2025-12-07 17:57:58 +0000

    Add files via upload

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+const hamMenu = document.querySelector("ham-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
